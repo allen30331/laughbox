5377c801ddf3a3687cdc0cc0ccb0a7f370a8aa2a
allen30331 <allen30331@gmail.com>
2017-03-26 19:59:00 -0400

    added cancel button to form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {reduxForm} from 'redux-form';
 import {createPost} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsNew extends Component {
 	render() {
@@ -11,20 +12,21 @@ class PostsNew extends Component {
 				<h3>Tell a joke</h3>
 				<div className="form-group">
 					<label>Title</label>
-					<input type="text" className="form-control" {...title} />
+					<input type="text" className="form-control" {...title} required />
 				</div>
 
 				<div className="form-group">
 					<label>Categories</label>
-					<input type="text" className="form-control" {...categories}/>
+					<input type="text" className="form-control" {...categories} required />
 				</div>
 
 				<div className="form-group">
 					<label>Content</label>
-					<textarea className="form-control" {...content}/>
-
-					<button type="submit" className="btn btn-primary">Submit</button>
+					<textarea className="form-control" {...content} required />
 				</div>
+
+				<button type="submit" className="btn btn-primary">Submit</button>
+				<Link to="/" className="btn btn-danger">Cancel</Link>
 			</form>
 		);
 	}
