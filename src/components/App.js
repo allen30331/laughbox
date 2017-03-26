eb68d5ad246487d88ecdc07e0bd08d115803804f
allen30331 <allen30331@gmail.com>
2017-03-26 18:19:11 -0400

    first push

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+       <div>
+		      <header>
+		      	<div className="row">
+		      		<div className="col-md-12">
+		      			<h3>laugh box</h3>
+		      		</div>
+		      	</div>
+			  </header>
+			  
+			  <div>
+			  {this.props.children}
+			  </div>
+	      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/reducers/reducer_posts.js
@@ -0,0 +1,12 @@
+import {FETCH_POSTS} from '../actions/index';
+
+const Initial_State = { all:[], post: null };
+
+export default function(state = Initial_State, action) {
+	switch(action.type) {
+		case FETCH_POSTS:
+			return {...state, all: action.payload.data};
+		default:
+			return state; 
+	}
+}
