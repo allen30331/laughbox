cdaffb8d0be36f7cf7ea9d6f80288dd34a5ef730
allen30331 <allen30331@gmail.com>
2017-03-26 19:27:30 -0400

    created input fields in posts new file

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -4,7 +4,25 @@ import {reduxForm} from 'redux-form';
 class PostsNew extends Component {
 	render() {
 		return (
-			<div>Create Form</div>
+			<form>
+				<h3>Tell a joke</h3>
+				<div className="form-group">
+					<label>Title</label>
+					<input type="text" className="form-control" />
+				</div>
+
+				<div className="form-group">
+					<label>Categories</label>
+					<input type="text" className="form-control" />
+				</div>
+
+				<div className="form-group">
+					<label>Content</label>
+					<textarea className="form-control" />
+
+					<button type="submit" className="btn btn-primary">Submit</button>
+				</div>
+			</form>
 		);
 	}
 }
