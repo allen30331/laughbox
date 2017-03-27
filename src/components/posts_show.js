8a6e0c3d764211c22aca9e75ba080407c13420c0
allen30331 <allen30331@gmail.com>
2017-03-26 21:31:49 -0400

    added link to go back to index

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {fetchPost} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsShow extends Component {
 
@@ -20,6 +21,7 @@ class PostsShow extends Component {
 		
 		return (
 			<div>
+				<Link to="/">back to jokes</Link>
 				<h3>{post.title}</h3>
 				<h6>Categories: {post.categories}</h6>
 				<p>{post.content}</p>
