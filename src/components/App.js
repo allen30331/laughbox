f99ff08667fea0ba4f3fca0bdd8a9f3d3c69fa20
allen30331 <allen30331@gmail.com>
2017-03-28 12:44:56 -0400

    added test for app component

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,19 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-       <div>
-		      <header>
-		      	<div className="row">
-		      		<div className="col-md-12">
-		      			<h3>laugh box</h3>
-		      		</div>
-		      	</div>
-			  </header>
-			  
-			  <div>
-			  {this.props.children}
-			  </div>
-	      </div>
+       <div className="app-container">
+	      <header>
+	      	<div className="row">
+	      		<div className="col-md-12">
+	      			<h3>laugh box</h3>
+	      		</div>
+	      	</div>
+		  </header>
+		  
+		  <div>
+		  	{this.props.children}
+		  </div>
+	    </div>
     );
   }
 }
--- test/App.test.js
+++ test/App.test.js
@@ -1,9 +1,20 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import App from '../src/App';
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+import chai from 'chai';
+const should = chai.should();
+import App from '../src/components/App';
 
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
+
+describe('App', function() {
+   it('Renders without breaking', function() {
+     const renderer = TestUtils.createRenderer();
+     renderer.render(<App />);
+     const result = renderer.getRenderOutput();
+
+     result.type.should.equal('div');
+     result.props.className.should.equal('app-container');
+  });
+ });
+
+
 
