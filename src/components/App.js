1f4de76fd1de4655cc97d16ad9612cf738949e4d
allen30331 <allen30331@gmail.com>
2017-03-29 00:16:22 -0400

    added css to header

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
+// import css from '../index.css';
+
 
 class App extends Component {
   render() {
     return (
        <div className="app-container">
-	      <header>
-	      	<div className="row">
-	      		<div className="col-md-12">
-	      			<h3>laugh box</h3>
-	      		</div>
-	      	</div>
-		  </header>
-		  
+	      <div className="header-container">
+		      <header>
+		      	<div className="row">
+		      		<div className="col-md-12">
+		      			<h3>laugh box</h3>
+		      		</div>
+		      	</div>
+			  </header>
+		  </div>
 		  <div>
 		  	{this.props.children}
 		  </div>
